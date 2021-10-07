97dedf8cf3fccd57aca6dd8e257d5ace8398dd22
Yadier Quesada <yadierq87@gmail.com>
2021-10-07 19:43:08 -0400

    [ADD] first commit uploading files

---
+++ README.rst
@@ -0,0 +1,13 @@
+=======================
+Crm Social Profile
+=======================
+Authors
+=======================
+Ing. Yadier Abel De Quesada Ricardo
+
+CRM social profile is created for register social accounts for customer in CRM module and
+see the information in different tabs.
+Module was develop for Odoo version 13
+
+This module can download or clone from  `<https://github.com/yadierq87/proyects-odoo>`_ project on GitHub.
+
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+{
+    "name": "Crm Social Profile",
+    "summary": """
+        This module allows registering social accounts for each customer.""",
+    "version": "1.0.0",
+    "license": "AGPL-3",
+    "author": "Yadier Abel De Quesada, Solprob",
+    "website": "https://solprob.nat.cu",
+    "application": False,
+    "installable": True,
+    "depends": ["base", "crm", "website"],
+    "data": [
+        "views/crm_social_profile.xml",
+        "views/crm_customer_template.xml",
+    ],
+    "demo": [],
+}
